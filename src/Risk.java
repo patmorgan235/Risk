816f635bd4f0f7be305105b2fe06d639237704fa
Patric Morgan <patric.morgan1@gmail.com>
2015-02-27 19:59:54 -0600

    added Risk map display Image(Broken)

--- src/Risk.java
+++ src/Risk.java
@@ -1,9 +1,38 @@
+import java.awt.*;
 
-public class Risk {
+@SuppressWarnings("serial")
+public class Risk extends GameEngine{
+	Img DispMap ;
+	
+	
+	public Risk(){
+		super("Risk",1200,612);
+		
+		DispMap = new Img("RiskMap.png");
+		DispMap.setPosition(100, 100);
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void draw(Graphics g){
+		if(DispMap == null)return;
+		
+		DispMap.draw(g);
+		
+	}
+	
+	public void processKey(int code, boolean pressed){
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new Risk();
 
 	}
+	
+	
 
 }
